61ec15877bb43bc0ff1b0be6e60cf6b6d3d799ca
Yeongdong <jyd37855@gmail.com>
2025-05-04 14:30:33 +0900

    test: (Service) add transaction commit, rollback, dispose test

---
+++ StockTrading.Tests/Implementations/DbTransactionWrapperTest.cs
@@ -0,0 +1,52 @@
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Storage;
+using Moq;
+using StockTrading.Infrastructure.Implementations;
+
+namespace StockTrading.Tests.Implementations;
+
+[TestSubject(typeof(DbTransactionWrapper))]
+public class DbTransactionWrapperTest
+{
+    [Fact]
+    public async Task CommitAsync_ShouldCallTransactionCommit()
+    {
+        var mockTransaction = new Mock<IDbContextTransaction>();
+        mockTransaction.Setup(t => t.CommitAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        var transactionWrapper = new DbTransactionWrapper(mockTransaction.Object);
+
+        await transactionWrapper.CommitAsync();
+
+        mockTransaction.Verify(t => t.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task RollbackAsync_ShouldCallTransactionRollback()
+    {
+        var mockTransaction = new Mock<IDbContextTransaction>();
+        mockTransaction.Setup(t => t.RollbackAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        var transactionWrapper = new DbTransactionWrapper(mockTransaction.Object);
+
+        await transactionWrapper.RollbackAsync();
+
+        mockTransaction.Verify(t => t.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task DisposeAsync_ShouldCallTransactionDispose()
+    {
+        var mockTransaction = new Mock<IDbContextTransaction>();
+        mockTransaction.Setup(t => t.DisposeAsync())
+            .Returns(ValueTask.CompletedTask);
+
+        var transactionWrapper = new DbTransactionWrapper(mockTransaction.Object);
+
+        await transactionWrapper.DisposeAsync();
+
+        mockTransaction.Verify(t => t.DisposeAsync(), Times.Once);
+    }
+}
